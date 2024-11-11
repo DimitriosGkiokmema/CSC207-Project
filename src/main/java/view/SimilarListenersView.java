9b38cde42c7766e212790aa3ba8609d6f077592d
KseniaTk <ksenia.tkaczyk@mail.utoronto.ca>
2024-11-11 14:41:31 -0500

    added changes to view

---
+++ src/main/java/view/SimilarListenersView.java
@@ -0,0 +1,14 @@
+package view;
+
+import javax.swing.*;
+
+public class SimilarListenersView {
+    private final SimilarListenersViewModel similarListenersViewModel;
+
+    public SimilarListenersView(SimilarListenersViewModel similarListenersViewModel) {
+        this.similarListenersViewModel = similarListenersViewModel;
+        final JLabel title = new JLabel("Similar Listeners");
+
+    }
+
+}

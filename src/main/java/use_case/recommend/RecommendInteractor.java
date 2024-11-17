1ecc3098761457cb254587b75413405eebdd7db4
Dimitrios <dimitrios.gkiokmema@mail.utoronto.ca>
2024-11-17 10:58:12 -0500

    Started working on interactor

--- src/main/java/use_case/recommend/RecommendInteractor.java
+++ src/main/java/use_case/recommend/RecommendInteractor.java
@@ -1,4 +1,32 @@
 package use_case.recommend;
 
-public class RecommendInteractor implements RecommendInputBoundary{
+import entity.User;
+
+/**
+ * The Recommendations Interactor.
+ */
+public class RecommendInteractor implements RecommendInputBoundary {
+    private final RecommendUserDataAcessInterface recommendDataAccessObject;
+    private final RecommendOutputBoundary recommendPresenter;
+
+    public RecommendInteractor(RecommendUserDataAcessInterface recommendDataAccessInterface,
+                           RecommendOutputBoundary recommendOutputBoundary) {
+        this.recommendDataAccessObject = recommendDataAccessInterface;
+        this.recommendPresenter = recommendOutputBoundary;
+    }
+
+    @Override
+    public void execute(RecommendInputData recommendInputData) {
+        final String username = recommendInputData.getLoginToken();
+        if (!recommendDataAccessObject.existsByName(username)) {
+            recommendPresenter.prepareFailView(username + ": Account does not exist.");
+        }
+        else {
+            final String accessToken = recommendDataAccessObject.get(username).getName();
+            final User user = userDataAccessObject.get(loginInputData.getLoginToken());
+            userDataAccessObject.setCurrentUsername(user.getName());
+            final LoginOutputData loginOutputData = new LoginOutputData(user.getName(), false);
+            loginPresenter.prepareSuccessView(loginOutputData);
+        }
+    }
 }

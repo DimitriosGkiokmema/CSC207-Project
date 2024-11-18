54de34749f53a0716bb6b7354de554c7cb947c43
KseniaTk <ksenia.tkaczyk@mail.utoronto.ca>
2024-11-18 13:33:17 -0500

    implemented SimilarListenersInputData

--- src/main/java/use_case/similar_listeners/SimilarListenersInputData.java
+++ src/main/java/use_case/similar_listeners/SimilarListenersInputData.java
@@ -1,4 +1,15 @@
 package use_case.similar_listeners;
 
 public class SimilarListenersInputData {
+
+    private final String loginToken;
+
+    public SimilarListenersInputData(String loginToken) {
+        this.loginToken = loginToken;
+    }
+
+    String getLoginToken() {
+        return loginToken;
+    }
+
 }

4936d348084d0421d90d177054d53539af12ee28
KseniaTk <ksenia.tkaczyk@mail.utoronto.ca>
2024-11-18 14:16:46 -0500

    implemented SimilarListenersController

--- src/main/java/interface_adapter/similar_listeners/SimilarListenersController.java
+++ src/main/java/interface_adapter/similar_listeners/SimilarListenersController.java
@@ -1,4 +1,24 @@
 package interface_adapter.similar_listeners;
 
+import use_case.similar_listeners.SimilarListenersInputBoundary;
+import use_case.similar_listeners.SimilarListenersInputData;
+
 public class SimilarListenersController {
+    private final SimilarListenersInputBoundary similarListenersInputBoundary;
+
+    public SimilarListenersController(SimilarListenersInputBoundary similarListenersUseCaseInteractor) {
+        this.similarListenersInputBoundary = similarListenersUseCaseInteractor;
+    }
+
+    /**
+     * Executes the Login Use Case.
+     * @param loginToken the username of the user logging in
+     */
+    public void execute(String loginToken) {
+        final SimilarListenersInputData similarListenersInputData = new SimilarListenersInputData(
+                loginToken);
+
+        similarListenersInputBoundary.execute(similarListenersInputData);
+    }
 }
+

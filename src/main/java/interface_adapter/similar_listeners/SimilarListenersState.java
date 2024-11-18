e937e93fbf92001d43745a66c6b12a8841a67e10
KseniaTk <ksenia.tkaczyk@mail.utoronto.ca>
2024-11-18 14:39:34 -0500

    implemented SimilarListenersState with needed methods

--- src/main/java/interface_adapter/similar_listeners/SimilarListenersState.java
+++ src/main/java/interface_adapter/similar_listeners/SimilarListenersState.java
@@ -1,4 +1,37 @@
 package interface_adapter.similar_listeners;
 
+import interface_adapter.logged_in.LoggedInState;
+
+import java.util.List;
+
 public class SimilarListenersState {
+    private List<String> similarArtists;
+    private String similarArtistsError;
+
+    public SimilarListenersState(SimilarListenersState copy) {
+        similarArtists = copy.similarArtists;
+        similarArtistsError = copy.similarArtistsError;
+    }
+
+    // Because of the previous copy constructor, the default constructor must be explicit.
+    public SimilarListenersState() {
+
+    }
+
+    public List<String> getSimilarArtists() {
+        return similarArtists;
+    }
+
+    public void setSimilarArtists(List<String> similarArtists) {
+        this.similarArtists = similarArtists;
+    }
+
+    public String getSimilarArtistsError() {
+        return similarArtistsError;
+    }
+
+    void setSimilarArtistsError(String similarArtistsError) {
+        this.similarArtistsError = similarArtistsError;
+    }
+
 }

c13c28169a900582bfc8ba24016b73b83ef6befd
KseniaTk <ksenia.tkaczyk@mail.utoronto.ca>
2024-11-18 13:52:54 -0500

    added constructor to Interactor

--- src/main/java/use_case/similar_listeners/SimilarListenersInteractor.java
+++ src/main/java/use_case/similar_listeners/SimilarListenersInteractor.java
@@ -1,4 +1,17 @@
 package use_case.similar_listeners;
 
-public class SimilarListenersInteractor {
+public class SimilarListenersInteractor implements SimilarListenersInputBoundary {
+    private final SimilarListenersUserDataAccessInterface userDataAccessObject;
+    private final SimilarListenersOutputBoundary loginPresenter;
+
+    public SimilarListenersInteractor(SimilarListenersUserDataAccessInterface userDataAccessInterface,
+                                      SimilarListenersOutputBoundary similarListenersOutputBoundary) {
+        this.userDataAccessObject = userDataAccessInterface;
+        this.loginPresenter = similarListenersOutputBoundary;
+    }
+
+    @Override
+    public void execute(SimilarListenersInputData similarListenersInputData) {
+
+    }
 }

7fc64d0e85a81d25bbe695e316ee5898dee542c5
KseniaTk <ksenia.tkaczyk@mail.utoronto.ca>
2024-11-18 13:39:08 -0500

    implemented SimilarListenersOutputData instance variables and constructor

--- src/main/java/use_case/similar_listeners/SimilarListenersOutputData.java
+++ src/main/java/use_case/similar_listeners/SimilarListenersOutputData.java
@@ -1,4 +1,16 @@
 package use_case.similar_listeners;
 
+import java.util.List;
+
 public class SimilarListenersOutputData {
+
+    private List<String> similarArtists;
+
+    public SimilarListenersOutputData(List<String> similarArtists) {
+        this.similarArtists = similarArtists;
+    }
+
+    public List<String> getSimilarArtists() {
+        return similarArtists;
+    }
 }

0bcc43b37ebbd0b81fab38de3a90cc7fa3e386ea
KseniaTk <ksenia.tkaczyk@mail.utoronto.ca>
2024-11-21 10:42:02 -0500

    added javadoc

--- src/main/java/interface_adapter/similar_listeners/SimilarListenersController.java
+++ src/main/java/interface_adapter/similar_listeners/SimilarListenersController.java
@@ -3,6 +3,9 @@ package interface_adapter.similar_listeners;
 import use_case.similar_listeners.SimilarListenersInputBoundary;
 import use_case.similar_listeners.SimilarListenersInputData;
 
+/**
+ * The controller for SimilarListeners use case.
+ */
 public class SimilarListenersController {
     private final SimilarListenersInputBoundary similarListenersInputBoundary;
 

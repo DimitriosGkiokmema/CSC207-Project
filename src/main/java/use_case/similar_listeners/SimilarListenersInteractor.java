22c0fda12faa33f4a08c9b2f5a035d9b1e873381
KseniaTk <ksenia.tkaczyk@mail.utoronto.ca>
2024-11-21 11:06:17 -0500

    implemented execute method

--- src/main/java/use_case/similar_listeners/SimilarListenersInteractor.java
+++ src/main/java/use_case/similar_listeners/SimilarListenersInteractor.java
@@ -4,17 +4,27 @@ import java.util.List;
 
 public class SimilarListenersInteractor implements SimilarListenersInputBoundary {
     private final SimilarListenersUserDataAccessInterface userDataAccessObject;
-    private final SimilarListenersOutputBoundary loginPresenter;
+    private final SimilarListenersOutputBoundary similarListenersPresenter;
 
     public SimilarListenersInteractor(SimilarListenersUserDataAccessInterface userDataAccessInterface,
                                       SimilarListenersOutputBoundary similarListenersOutputBoundary) {
         this.userDataAccessObject = userDataAccessInterface;
-        this.loginPresenter = similarListenersOutputBoundary;
+        this.similarListenersPresenter = similarListenersOutputBoundary;
     }
 
     @Override
     public void execute(SimilarListenersInputData similarListenersInputData) {
-
+        final String accessToken = similarListenersInputData.getAccessToken();
+        if (userDataAccessObject.getFollowedArtists().isEmpty()) {
+            similarListenersPresenter.prepareFailView("You do not follow any artists. Similar Listeners cannot be determined.");
+        }
+        else {
+            final List<String> followedArtists = userDataAccessObject.getFollowedArtists();
+            userDataAccessObject.setCurrentFollowedArtists(followedArtists);
+            final SimilarListenersOutputData similarListenersOutputData = new SimilarListenersOutputData(followedArtists, false);
+            similarListenersPresenter.prepareSuccessView(similarListenersOutputData);
+        }
+    }
 
     }
-}
+

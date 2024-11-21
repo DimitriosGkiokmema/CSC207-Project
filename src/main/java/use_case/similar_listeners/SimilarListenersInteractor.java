5321f3702c90fd00362d5ab5f4a699b5b36bccfa
KseniaTk <ksenia.tkaczyk@mail.utoronto.ca>
2024-11-21 11:15:00 -0500

    updated execute method

--- src/main/java/use_case/similar_listeners/SimilarListenersInteractor.java
+++ src/main/java/use_case/similar_listeners/SimilarListenersInteractor.java
@@ -2,6 +2,9 @@ package use_case.similar_listeners;
 
 import java.util.List;
 
+/**
+ * SimilarListeners Interactor.
+ */
 public class SimilarListenersInteractor implements SimilarListenersInputBoundary {
     private final SimilarListenersUserDataAccessInterface userDataAccessObject;
     private final SimilarListenersOutputBoundary similarListenersPresenter;
@@ -14,17 +17,19 @@ public class SimilarListenersInteractor implements SimilarListenersInputBoundary
 
     @Override
     public void execute(SimilarListenersInputData similarListenersInputData) {
-        final String accessToken = similarListenersInputData.getAccessToken();
+        // final String accessToken = similarListenersInputData.getAccessToken();
         if (userDataAccessObject.getFollowedArtists().isEmpty()) {
-            similarListenersPresenter.prepareFailView("You do not follow any artists. Similar Listeners cannot be determined.");
+            similarListenersPresenter.prepareFailView("You do not follow any artists. "
+                    + "Similar Listeners cannot be determined.");
         }
         else {
             final List<String> followedArtists = userDataAccessObject.getFollowedArtists();
             userDataAccessObject.setCurrentFollowedArtists(followedArtists);
-            final SimilarListenersOutputData similarListenersOutputData = new SimilarListenersOutputData(followedArtists, false);
+            final SimilarListenersOutputData similarListenersOutputData = new SimilarListenersOutputData(
+                    followedArtists, false);
             similarListenersPresenter.prepareSuccessView(similarListenersOutputData);
         }
     }
 
-    }
+}
 

e559c9c939ec96e21b36771cf39ff8f569c7428d
KseniaTk <ksenia.tkaczyk@mail.utoronto.ca>
2024-11-21 11:15:56 -0500

    added javadoc

--- src/main/java/use_case/similar_listeners/SimilarListenersInputData.java
+++ src/main/java/use_case/similar_listeners/SimilarListenersInputData.java
@@ -3,6 +3,9 @@ package use_case.similar_listeners;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The input data for SimilarListeners use case.
+ */
 public class SimilarListenersInputData {
 
     private final String accessToken;

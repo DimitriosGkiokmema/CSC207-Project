3d3e2499eb089478b84b95051137d019290318d7
KseniaTk <ksenia.tkaczyk@mail.utoronto.ca>
2024-11-21 11:02:20 -0500

    added instance variable

--- src/main/java/use_case/similar_listeners/SimilarListenersInputData.java
+++ src/main/java/use_case/similar_listeners/SimilarListenersInputData.java
@@ -1,15 +1,25 @@
 package use_case.similar_listeners;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SimilarListenersInputData {
 
     private final String accessToken;
+    private final List<String> artistsFollowed;
 
-    public SimilarListenersInputData(String accessToken) {
+    public SimilarListenersInputData(String accessToken, ArrayList<String> artistsFollowed) {
+
         this.accessToken = accessToken;
+        this.artistsFollowed = artistsFollowed;
     }
 
-    String getLoginToken() {
+    String getAccessToken() {
         return accessToken;
+    }
+
+    List<String> getArtistsFollowed() {
+        return artistsFollowed;
     }
 
 }

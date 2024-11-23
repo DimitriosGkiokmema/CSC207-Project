70d674767c67c237c6389b7c0ee506a2de145f17
KseniaTk <ksenia.tkaczyk@mail.utoronto.ca>
2024-11-23 17:29:53 -0500

    added success test

--- src/test/java/use_case/similar_listeners/SimilarListenersInteractorTest.java
+++ src/test/java/use_case/similar_listeners/SimilarListenersInteractorTest.java
@@ -1,11 +1,14 @@
 package use_case.similar_listeners;
 
 import data_access.InMemoryUserDataAccessObject;
+import data_access.SimilarListenersTestDataAccessObject;
 import entity.CommonUserFactory;
 import entity.User;
 import entity.UserFactory;
 import org.junit.jupiter.api.Test;
-import use_case.logout.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,21 +16,25 @@ public class SimilarListenersInteractorTest {
 
     @Test
     void successTest() {
-        SimilarListenersInputData inputData = new SimilarListenersInputData("BQCOFgyHIg-eAjqOM8ITkCEynoZwEoN2__Sccimaakn2rZjBzIBTwSVK391IUAD7umWe6vvEE9MiiqvN6j1jFySdxecxQL0gS1Tt2y3DAlrt6c6XlfD6jEQJPiNqyOHueQr9ZQe4Um0A6Bz2cbKvzi-fW1MQG19K2rmT_Z0rLFQY-p3S9ssF-5213Ol_hAvpB0kORgMoazMdzPYe6A");
-        InMemoryUserDataAccessObject userRepository = new InMemoryUserDataAccessObject();
+        SimilarListenersInputData inputData = new SimilarListenersInputData("token");
+        SimilarListenersTestDataAccessObject accessObject = new SimilarListenersTestDataAccessObject();
 
         // For the success test, we need to add Paul to the data access repository before we log in.
-        UserFactory factory = new CommonUserFactory();
-        User user = factory.create("Paul", "password");
-        userRepository.save(user);
-        userRepository.setCurrentUsername("Paul");
+        List<String> allfollowedArtists = new ArrayList<>();
+        allfollowedArtists.add("Michael Jackson");
+        allfollowedArtists.add("Drake");
+        allfollowedArtists.add("Madonna");
+        allfollowedArtists.add("Lady Gaga");
+        allfollowedArtists.add("The Police");
+        accessObject.setCurrentFollowedArtists(allfollowedArtists);
 
         // This creates a successPresenter that tests whether the test case is as we expect.
-        LogoutOutputBoundary successPresenter = new LogoutOutputBoundary() {
+        SimilarListenersOutputBoundary successPresenter = new SimilarListenersOutputBoundary() {
             @Override
-            public void prepareSuccessView(LogoutOutputData user) {
+            public void prepareSuccessView(SimilarListenersOutputData outputData) {
                 // check that the output data contains the username of who logged out
-                assertEquals("Paul", user.getUsername());
+                assertEquals(allfollowedArtists, outputData.getSimilarArtists());
+                assertEquals("Madonna", outputData.getSimilarArtists().get(2));
             }
 
             @Override
@@ -36,10 +43,8 @@ public class SimilarListenersInteractorTest {
             }
         };
 
-        LogoutInputBoundary interactor = new LogoutInteractor(userRepository, successPresenter);
+        SimilarListenersInputBoundary interactor = new SimilarListenersInteractor(accessObject, successPresenter);
         interactor.execute(inputData);
-        // check that the user was logged out
-        assertNull(userRepository.getCurrentUsername());
     }
 
 }

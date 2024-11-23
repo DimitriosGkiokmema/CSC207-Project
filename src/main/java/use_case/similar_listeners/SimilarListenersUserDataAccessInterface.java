f75d14b922650fbb0864868d71c05d2c876857f1
KseniaTk <ksenia.tkaczyk@mail.utoronto.ca>
2024-11-22 19:06:03 -0500

    updated

--- src/main/java/use_case/similar_listeners/SimilarListenersUserDataAccessInterface.java
+++ src/main/java/use_case/similar_listeners/SimilarListenersUserDataAccessInterface.java
@@ -2,32 +2,21 @@ package use_case.similar_listeners;
 
 import java.util.List;
 
-import entity.User;
-
 /**
  * DAO for SimilarListeners use case.
  */
 public interface SimilarListenersUserDataAccessInterface {
 
     /**
-     * Returns the user with the given username.
-     * @param username the username to look up
-     * @return the user with the given username
-     */
-    User get(String username);
-
-    /**
-     * Returns the access token of the curren user of the application.
-     * @return the access Token of the current user; null indicates that no one is logged into the application.
-     */
-    String getCurrentAccessToken();
-
-    /**
      * Return the artists that the current user follows.
-     * @return list of artists that this user follows.
+     * @return list of artists that this user follows. Return null if there are no followed artists.
      */
     List<String> getFollowedArtists();
 
+    /**
+     * Sets the users followed artists.
+     * @param followedArtists is the list of artists names.
+     */
     void setCurrentFollowedArtists(List<String> followedArtists);
 
 

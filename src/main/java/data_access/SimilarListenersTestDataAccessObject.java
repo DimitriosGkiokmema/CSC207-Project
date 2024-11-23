e655754c2064851aceb5a01c69c4d4a1a74d5d3c
KseniaTk <ksenia.tkaczyk@mail.utoronto.ca>
2024-11-23 17:30:10 -0500

    created access object for testing

--- src/main/java/data_access/SimilarListenersTestDataAccessObject.java
+++ src/main/java/data_access/SimilarListenersTestDataAccessObject.java
@@ -6,14 +6,16 @@ import java.util.List;
 
 public class SimilarListenersTestDataAccessObject implements SimilarListenersUserDataAccessInterface {
 
+    private List<String> followedArtists;
 
     @Override
     public List<String> getFollowedArtists() {
-
+        return this.followedArtists;
     }
 
     @Override
-    public void setCurrentFollowedArtists(List<String> followedArtists) {
+    public void setCurrentFollowedArtists(List<String> artists) {
+        this.followedArtists = artists;
 
     }
 }

ab087aabe4acbdad32dfd2bbafdfc468c76608f9
KseniaTk <ksenia.tkaczyk@mail.utoronto.ca>
2024-11-22 19:05:54 -0500

    created access object for testing

---
+++ src/main/java/data_access/SimilarListenersTestDataAccessObject.java
@@ -0,0 +1,19 @@
+package data_access;
+
+import use_case.similar_listeners.SimilarListenersUserDataAccessInterface;
+
+import java.util.List;
+
+public class SimilarListenersTestDataAccessObject implements SimilarListenersUserDataAccessInterface {
+
+
+    @Override
+    public List<String> getFollowedArtists() {
+
+    }
+
+    @Override
+    public void setCurrentFollowedArtists(List<String> followedArtists) {
+
+    }
+}

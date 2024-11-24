531386bb91a347061077f79d1a5fc88647d096e2
eulogizee <eulogizee@outlook.com>
2024-11-23 19:39:33 -0500

    imported ActionListener and ActionEvent class

--- src/main/java/view/LoggedInView.java
+++ src/main/java/view/LoggedInView.java
@@ -1,6 +1,8 @@
 package view;
 
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;

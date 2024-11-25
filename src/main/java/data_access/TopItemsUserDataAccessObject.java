786d03131bfb9df242d57cf858bdece91a3b6e08
ceryanki <yanki.cer@mail.utoronto.ca>
2024-11-25 12:16:37 -0500

    Created a dummy DataAccessObject constructor to be able to test whether the incoming data works.

--- src/main/java/data_access/TopItemsUserDataAccessObject.java
+++ src/main/java/data_access/TopItemsUserDataAccessObject.java
@@ -2,18 +2,28 @@ package data_access;
 
 import use_case.top_items.TopItemsUserDataAccessInterface;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TopItemsUserDataAccessObject implements TopItemsUserDataAccessInterface {
 
+    private List<String> tracks;
+
+    public TopItemsUserDataAccessObject() {
+        tracks = new ArrayList<>();
+        tracks.add("Heartbreaker");
+        tracks.add("reincarnated");
+        tracks.add("Kill Bill");
+        tracks.add("Crew Love");
+    }
+
     @Override
     public List<String> getCurrentTopTracks() {
-        return List.of();
+        return tracks;
     }
 
     @Override
     public void setCurrentTopTracks(List<String> tracks) {
-
     }
 
     @Override

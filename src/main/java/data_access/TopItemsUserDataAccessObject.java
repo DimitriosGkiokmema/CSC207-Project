cff3349f509acebe54aaab00bc93755e5679efa8
ceryanki <yanki.cer@mail.utoronto.ca>
2024-11-26 19:31:19 -0500

    Added some dummmy tests.

--- src/main/java/data_access/TopItemsUserDataAccessObject.java
+++ src/main/java/data_access/TopItemsUserDataAccessObject.java
@@ -8,13 +8,19 @@ import java.util.List;
 public class TopItemsUserDataAccessObject implements TopItemsUserDataAccessInterface {
 
     private List<String> tracks;
+    private List<String> artists;
 
     public TopItemsUserDataAccessObject() {
         tracks = new ArrayList<>();
+        artists = new ArrayList<>();
         tracks.add("Heartbreaker");
         tracks.add("reincarnated");
         tracks.add("Kill Bill");
         tracks.add("Crew Love");
+        artists.add("The Weeknd");
+        artists.add("Kanye West");
+        artists.add("A$AP Rocky");
+
     }
 
     @Override
@@ -28,7 +34,7 @@ public class TopItemsUserDataAccessObject implements TopItemsUserDataAccessInter
 
     @Override
     public List<String> getCurrentTopArtists() {
-        return List.of();
+        return artists;
     }
 
     @Override

ae9168afe983311b5730011d0639b091de71a7ea
ceryanki <yanki.cer@mail.utoronto.ca>
2024-11-29 14:39:13 -0500

    Created a FailView for the Top Items use case.

--- src/main/java/interface_adapter/top_items/TopItemsPresenter.java
+++ src/main/java/interface_adapter/top_items/TopItemsPresenter.java
@@ -4,6 +4,8 @@ import interface_adapter.ViewManagerModel;
 import use_case.top_items.TopItemsOutputBoundary;
 import use_case.top_items.TopItemsOutputData;
 
+import java.util.ArrayList;
+
 /**
  * The Presenter for the Top Tracks Use Case.
  */
@@ -34,6 +36,13 @@ public class TopItemsPresenter implements TopItemsOutputBoundary {
 
     @Override
     public void prepareFailView(String errorMessage) {
-        // No code needed because there won't be an error.
+        final TopItemsState topItemsState = topItemsViewModel.getState();
+        topItemsState.setTracksError(errorMessage);
+        topItemsState.setArtistsError(errorMessage);
+        topItemsState.setTracks(new ArrayList<>());
+        topItemsState.setArtists(new ArrayList<>());
+
+        this.viewManagerModel.setState(topItemsViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
     }
 }

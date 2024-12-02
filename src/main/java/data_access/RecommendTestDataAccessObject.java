646474c656038f49f77081e6df82fabcf7be051c
Dimitrios Gkiokmema <156963228+DimitriosGkiokmema@users.noreply.github.com>
2024-12-02 18:45:30 -0500

    Update RecommendTestDataAccessObject.java

--- src/main/java/data_access/RecommendTestDataAccessObject.java
+++ src/main/java/data_access/RecommendTestDataAccessObject.java
@@ -1,11 +1,13 @@
 package data_access;
 
 import use_case.recommend.RecommendDataAccessInterface;
+import use_case.recommend.RecommendLanguageModelDataAccessInterface;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class RecommendTestDataAccessObject implements RecommendDataAccessInterface {
+public class RecommendTestDataAccessObject implements RecommendDataAccessInterface,
+        RecommendLanguageModelDataAccessInterface {
     private List<String> topTracks;
     private List<String> topArtists;
 
@@ -27,26 +29,27 @@ public class RecommendTestDataAccessObject implements RecommendDataAccessInterfa
     }
 
     @Override
-    public List<String> getCurrentTopTracks2() {
+    public List<String> getCurrentTopTracks() {
         return topTracks;
     }
 
     @Override
-    public void setCurrentTopTracks2(List<String> topTracks) {
+    public void setCurrentTopTracks(List<String> topTracks) {
         this.topTracks = topTracks;
     }
 
     @Override
-    public List<String> getCurrentTopArtists2() {
+    public List<String> getCurrentTopArtists() {
         return topArtists;
     }
 
     @Override
-    public void setCurrentTopArtists2(List<String> topArtists) {
+    public void setCurrentTopArtists(List<String> topArtists) {
         this.topArtists = topArtists;
     }
 
     @Override
-    public void setAccessToken(String accessToken) {
+    public String getRecommendations(List<String> prompt, List<String> topArtists) {
+        return "This is a sample response from Azure";
     }
 }

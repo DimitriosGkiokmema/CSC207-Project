2e7b4b423b9f9ae3ebf7acd6696ad41c48fa93ad
Dimitrios Gkiokmema <156963228+DimitriosGkiokmema@users.noreply.github.com>
2024-12-02 18:50:03 -0500

    Cleaning up code

--- src/main/java/use_case/recommend/RecommendDataAccessInterface.java
+++ src/main/java/use_case/recommend/RecommendDataAccessInterface.java
@@ -10,25 +10,23 @@ public interface RecommendDataAccessInterface {
      * Returns the top track list of the current user of the application.
      * @return the top track List of the current user; null indicates that there are no track lists.
      */
-    List<String> getCurrentTopTracks2();
+    List<String> getCurrentTopTracks();
 
     /**
      * Sets the top tracks list indicating who is the current user of the application.
      * @param tracks the new top track list; null to indicate that there is no top tracks.
      */
-    void setCurrentTopTracks2(List<String> tracks);
+    void setCurrentTopTracks(List<String> tracks);
 
     /**
      * Returns the top artist list of the current user of the application.
      * @return the top artist list of the current user; null indicates that there are no artist lists.
      */
-    List<String> getCurrentTopArtists2();
+    List<String> getCurrentTopArtists();
 
     /**
      * Sets the top artist list indicating who is the current user of the application.
      * @param artists the new top artist list; null to indicate that there is no top tracks.
      */
-    void setCurrentTopArtists2(List<String> artists);
-
-    void setAccessToken(String accessToken);
+    void setCurrentTopArtists(List<String> artists);
 }

46925e485bab2f5b64e35087ab7473e13a83e9b5
eulogizee <eulogizee@outlook.com>
2024-12-02 16:31:58 -0500

    added test for keyword use case

---
+++ src/main/java/data_access/KeywordTestDataAccessObject.java
@@ -0,0 +1,23 @@
+package data_access;
+
+import use_case.keyword.KeywordDataAccessInterface;
+import java.util.ArrayList;
+import java.util.List;
+
+public class KeywordTestDataAccessObject implements KeywordDataAccessInterface {
+
+    @Override
+    public List<String> fetchSongs(String artist, String keyword) {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<String> searchSongs(String artistName, String keyword) {
+        if ("Taylor Swift".equals(artistName) && "Love".equals(keyword)) {
+            List<String> results = new ArrayList<>();
+            results.add("Love Story");
+            return results;
+        }
+        return new ArrayList<>();
+    }
+}
